7bed44c49fae4d4e2a3655ab2259403e4bd0144d
Sarah Garlock <garlock.sarah10@gmail.com>
2023-05-19 15:51:08 -0600

    add validations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,11 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true, numericality: true
+  validates :description, presence: true
+  validates :pet_names, presence: true
 end
