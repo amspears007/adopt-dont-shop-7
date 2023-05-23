6452f69b98b2e91cfa4f201947f29d2755c062d9
Amy Spears <amspears007@gmail.com>
2023-05-22 18:56:19 -0600

    Add status to pet_application table and update show and upadte method to find the petapplication

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -1,20 +1,18 @@
 class Admin::ApplicationsController < ApplicationController
-  def show
+  def show    
     @application = Application.find(params[:id])
-    @pet= Pet.find_by(params[:pet_id])
+    @pet_applications = @application.pet_applications
   end
-
+  
   def update
-    @approved_application = Application.find(params[:id])
-    @approved_pet= Pet.find_by(params[:pet_id])
-    
-    # if params[:status] == "Approved"
-    #   application.update(status: params[:status])
-    #   require 'pry'; binding.pry
-    # else
-    #   pet_app = PetApplication.create!(application_id: application.id, pet_id: @approved_pet.id)
-    # end
-      redirect_to "/admin/applications/#{application.id}"
+    approved_application = PetApplication.find_by(application_id: params[:id], pet_id: params[:pet_id])
+    approved_pet= Pet.find(params[:pet_id])
+    if params[:status] == "Approved"
+      approved_application.update(status: params[:status])
+    else
+      pet_app = PetApplication.create!(application_id: approved_application.id, pet_id: approved_pet.id)
+    end
+      redirect_to "/admin/applications/#{params[:id]}"
   end
 end
 
